c0c2c220cd44408635e06ed458804cde75f192b5
hfs <hfssiddiqui@gmail.com>
2020-09-03 13:34:20 +0530

    creating a dist folder

---
+++ build/precache-manifest.09828a59bab9aaf7db16841425392a27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d8505c4037e2f778bd8ed493af3e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5622d7ed4bee05fac756",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a92d30c9074cb1f213ef",
+    "url": "/static/js/2.b54d2062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b54d2062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5622d7ed4bee05fac756",
+    "url": "/static/js/main.d5ef9269.chunk.js"
+  },
+  {
+    "revision": "25632d1d7795a2d0e90f",
+    "url": "/static/js/runtime-main.83e7662c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
