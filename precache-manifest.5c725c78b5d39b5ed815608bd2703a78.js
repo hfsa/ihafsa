c84e2110dbcef4a54258133122d0b822611a0d40
hfs <hfssiddiqui@gmail.com>
2020-09-07 15:25:16 +0530

    Updates

---
+++ precache-manifest.5c725c78b5d39b5ed815608bd2703a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46179be777c1730c411739b4c94a8837",
+    "url": "/ihafsa/index.html"
+  },
+  {
+    "revision": "56464d88f0dfbbcddbb8",
+    "url": "/ihafsa/static/css/main.893d4c11.chunk.css"
+  },
+  {
+    "revision": "7b125c8840f9123910b0",
+    "url": "/ihafsa/static/js/2.2244ff31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ihafsa/static/js/2.2244ff31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56464d88f0dfbbcddbb8",
+    "url": "/ihafsa/static/js/main.3f1a31c0.chunk.js"
+  },
+  {
+    "revision": "fb02c1f8d2c01eec448e",
+    "url": "/ihafsa/static/js/runtime-main.c535a518.js"
+  }
+]);
