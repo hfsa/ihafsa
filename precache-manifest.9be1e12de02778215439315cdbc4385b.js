68be73e9978e9ed8131164d1376dca3919785cdf
hfs <hfssiddiqui@gmail.com>
2020-09-07 15:39:27 +0530

    Updates

---
+++ precache-manifest.9be1e12de02778215439315cdbc4385b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d701159362eae67886cd2535e1849745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69bfd48fbe698442215",
+    "url": "/static/css/main.e427dfa7.chunk.css"
+  },
+  {
+    "revision": "7b125c8840f9123910b0",
+    "url": "/static/js/2.2244ff31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2244ff31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69bfd48fbe698442215",
+    "url": "/static/js/main.6428d5d5.chunk.js"
+  },
+  {
+    "revision": "25632d1d7795a2d0e90f",
+    "url": "/static/js/runtime-main.83e7662c.js"
+  }
+]);
