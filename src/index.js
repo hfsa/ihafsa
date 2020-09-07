23021afe17035998d76e030ae5042b45f1d39241
hfs <hfssiddiqui@gmail.com>
2020-09-07 15:36:21 +0530

    styling the app2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// //import './index.css';
+// import App from './App';
+// import "./styles.css";
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   //<React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./styles.css";
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// pay attention to this. the first param for ReadtDom.render
+// is our initial file/app/component name, and second param
+// is the root element/div from index.html file
+ReactDOM.render(<App />, rootElement);
